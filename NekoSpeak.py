a2c94ec024cb3179bd12ae10b7caba5e61a7ad47
homayk228 <43516026+SouceCalve@users.noreply.github.com>
2025-10-08 20:54:22 +0700

    [UPD] Changed word multiplicator
    Добавил дефисы в удлинённые буквы, чтобы звучало характернее.

--- NekoSpeak.py
+++ NekoSpeak.py
@@ -52,8 +52,8 @@ class NekoSpeakModule(loader.Module):
                 if word.lower().startswith("н") and len(word) > 2 and random.random() < 0.05:
                     word = "ня" + word[2:]
                 if random.random() < 0.05:
-                    word = word.replace("р", "р" * random.randint(2,4))
-                    word = word.replace("r", "r" * random.randint(2,4))
+                    word = word.replace("р", "р-" * random.randint(2,4)+"р")
+                    word = word.replace("r", "r" * random.randint(2,4)+"r")
                 new_words.append(word)
             text = " ".join(new_words)
 
